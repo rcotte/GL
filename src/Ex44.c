69b84b810146090484b36d64096d78bb8240a912
rcotte <raphael.cotte@u-psud.fr>
2018-11-13 23:28:21 +0100

    FIn du 45

---
+++ src/Ex44.c
@@ -0,0 +1,16 @@
+//  Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include "my_header.h"
+
+#include "TP_lib.h"
+
+void main(void)
+{
+	short resultat , i=0;
+	while(i<10)
+	{
+		resultat=LancerDe();
+		i++;
+	}
+}
