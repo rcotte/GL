54f142036b883fd9308381f9e1ae59ec2dd4c999
rcotte <raphael.cotte@u-psud.fr>
2018-11-13 22:18:25 +0100

    ex43

---
+++ src/EX43/ex43.c
@@ -0,0 +1,56 @@
+// eX43 PArtie 1//
+#include "my_header.h"
+
+int main (void)
+{
+	int S1=3, n = 0;
+	
+	while (n<146)
+	{
+		S1= 3+3*n;
+		
+		n = n+1;
+	}
+	return 0;
+	
+}							  
+
+// Ex43 Partie 2 //
+
+int main (void)
+{
+	int S2=3, n = 0;
+	
+	while (3*n<9876)
+	{
+		S2= 3+3*n;
+		
+		n = n+1;
+	}
+	return 0;
+	
+}				
+
+// Ex43 Partie 3 //
+
+#include <stdlib.h>
+
+int main (void)
+
+{
+	 int S3=0, n=0;
+			   
+	 while (n<12)
+	 {
+		 S3= S3+ rand()%10;
+	 	 n=n+1;
+	 }
+	 
+
+    return 0;
+}
+
+
+// Ex43 PArtie 4 //
+
+
