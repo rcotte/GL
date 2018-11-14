56821ee87bfe8ff974730ac0f010aea336fb1792
rcotte <raphael.cotte@u-psud.fr>
2018-11-14 18:59:29 +0100

    Amelioration

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 // eX43 PArtie 1//
 #include "my_header.h"
+#include <stdlib.h>
 
 int main (void)
 {
-	int S1=0, n = 0;
+	int S1=0, n = 0, S2=0,S3=0,S4=0,a=0;
 	
 	while (n>0)
 	{
@@ -11,34 +12,16 @@ int main (void)
 		
 		n = n+1;
 	}
-	return 0;
 	
-}							  
 
-// Ex43 Partie 2 //
 
-int main (void)
-{
-	int S2=0, n = 0;
-	
 	while (3*n<9876)
 	{
 		S2= S2 +n;
 		
 		n = n+1;
 	}
-	return 0;
 	
-}				
-
-// Ex43 Partie 3 //
-
-#include <stdlib.h>
-
-int main (void)
-
-{
-	 int S3=0, n=0;
 			   
 	 while (n<12)
 	 {
@@ -46,17 +29,9 @@ int main (void)
 	 	 n=n+1;
 	 }
 	 
-
-    return 0;
-}
-
-
-// Ex43 PArtie 4 //
-int main (void)
-{
-	int S4=0, n=0,a=0;
+	 
 	
-	while (a<50&& n<=20)
+	while (a<50 && n<=20)
 	{
 		if(a%3==0 || a%7==0)
 		{
