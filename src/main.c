921691e314b5fb0010a634b30f4f5694150bd293
raphael cotte <raphael.cotte@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:23:36 +0100

    1ere partie

--- src/main.c
+++ src/main.c
@@ -3,7 +3,34 @@
 
 #include "my_header.h"
 
+#define SEUIL  50
+
+
 int main(void)
+
 {
+	unsigned char bin[10]= {25, 120, 65, 85,12,89,200,6,245,180}; 
+	int i;
+	
+	for (i =0; i<10; i=i+1)
+		
+	{
+		if(bin[i] < SEUIL)
+		{
+			bin[i] = 0;
+		}
+		
+		else
+			bin[i]=1;
+		
+	}
+		
+		
+	
+	
+
+	
+	
+	
     return 0;
 }
