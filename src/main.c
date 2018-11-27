efdaf724a9347719b72b06d4da433fd80759c882
raphael cotte <raphael.cotte@ge1.iut-cachan.u-psud.fr>
2018-11-27 10:17:58 +0100

    jvbi

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 #define SEUIL  50
-
+#define SEUILB -100
+#define SEUILH 200
 
 int main(void)
 
 {
 	unsigned char bin[10]= {25, 120, 65, 85,12,89,200,6,245,180}; 
+	int sat[10]; 
 	int i;
 	
 	for (i =0; i<10; i=i+1)
@@ -24,13 +27,29 @@ int main(void)
 			bin[i]=1;
 		
 	}
+	
+	
+		
+	for (i =0; i<10; i=i+1)
+	{
+		if(sat[i]<SEUILB)
+			sat[i] = SEUILB;
+		
+		else if (sat[i]>SEUILH)
+				sat[i] = SEUILH;
+		
+		else 
+			sat[i] = sat[i];
 		
 		
+		
+	 return 0;
+	}
 	
 	
 
 	
 	
 	
-    return 0;
+   
 }
