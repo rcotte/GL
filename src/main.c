37cebf5b4f830ec4d2600279132a3d5f6a57ac29
raphael cotte <raphael.cotte@ge1.iut-cachan.u-psud.fr>
2018-11-27 11:35:55 +0100

    vcy

--- src/main.c
+++ src/main.c
@@ -3,16 +3,29 @@
 
 
 #include "my_header.h"
-
+#include "TP_lib_suite.h"
 
 
 int main(void)
   
     {
+	 int joeur [6]={tirernumero(),tirernumero(),tirernumero(),tirernumero(),tirernumero(),tirernumero()};
+	 initialiserTirage();
+	 int gagnante[6] ={tirernumero(),tirernumero(),tirernumero(),tirernumero(),tirernumero(),tirernumero()};
+	 int bon_numero=0;
+	 int i,m;
 	
-		
-		
-		
+	 
+	 
+	 for (i=0;i<6;i++)
+	 {
+		 for(m=0;m<6;m++)
+		 {
+			 if(joeur[i]==gagnante[m])
+				 bon_numero++;
+		 }
+	 }
+	 
 	 return 0;
 	
 	}
