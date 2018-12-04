f899259e9050e3f47949fa6cbbb421efd7a51aa0
raphael cotte <raphael.cotte@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:33:09 +0100

    exo79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
-    return 0;
+     char prenom[100];
+	 char nom[100];
+	 char age[100];
+	 
+    
+	printf("veuillez taper le prenom : ");
+	scanf ("%s",prenom);
+	printf("veuillez taper le nom : ");
+	scanf("%s",nom);
+	printf("veuillez taper l'age : ");
+	scanf("%s",age);
+	
+	printf("je m'appelle %s %s et j'ai %s ans",prenom, nom, age) ;
+	getchar();
+	
+	
+
 }
